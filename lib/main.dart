490d3f69f60e70f60a9d9a5f12d9d0548f516a13
Adiletich <adilet.azu@gmail.com>
2022-07-26 19:07:24 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Lesson1_5(),
+    );
+  }
+}
+
+class Lesson1_5 extends StatelessWidget {
+  const Lesson1_5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.blue,
+              width: 10,
+            ),
+            // borderRadius: BorderRadius.circular(20),
+            color: Colors.red[500],
+          ),
+          child: const Text(
+            'ITC Bootcamp',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
